78462324040ebf909f07d734232aa3b345808099
Ian Watson <ian@sweet.uk.net>
2012-01-12 00:24:57 +0000

    Removed url from socket connection call

--- js/magnets.js
+++ js/magnets.js
@@ -353,7 +353,7 @@ var socket = {
 		$.extend(socket.config, config);
 				
 		try {
-			socket.connection = io.connect(socket.config.host);
+			socket.connection = io.connect();
 			// Set initial not ready message
 			socket.log("event", "Socket status: closed");
 
